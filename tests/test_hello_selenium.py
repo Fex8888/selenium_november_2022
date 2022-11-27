46ff633a77c2b6214a55f3abaf0ab274264b9186
Fex8888 <102014053+Fex8888@users.noreply.github.com>
2022-11-27 20:50:29 +0300

    Update test_hello_selenium.py

--- tests/test_hello_selenium.py
+++ tests/test_hello_selenium.py
@@ -1,1 +1,7 @@
-import timedef test_hello_world(browser):    browser.get('https://ya.ru')    time.sleep(3)    assert 'Яндекс' in browser.title
+import time
+
+
+def test_hello_world(browser):
+    browser.get('https://ya.ru')
+    time.sleep(3)
+    assert 'Яндекс' in browser.title
